892bffc1c1ff80dd56fee98e3d7afa040d708e85
Bazze <bluebazze@gmail.com>
2020-11-02 23:29:50 +0100

    Neu4jTest commit
    Initial commit of Neo4jTest (.net Core Console app)

---
+++ Neo4jTest/Neo4jTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Neo4jTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
